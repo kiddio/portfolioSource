9cb8c75d7ec0291c29dfdb36199c029e8b49f0ca
Farhad Sharif <fader91095@gmail.com>
2020-05-10 14:18:48 +0100

    Working with react

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import {
+  Box,
+  Card,
+  Image,
+  Heading,
+  Text,
+  Button
+} from 'rebass'
+import { ThemeProvider } from 'emotion-theming'
+const theme = {
+  fontSizes: [
+    12, 14, 16, 24, 32, 48, 64
+  ],
+  colors: {
+    primary: '#07c',
+    gray: '#f6f6ff',
+  },
+  buttons: {
+    primary: {
+      color: 'white',
+      bg: 'primary',
+    },
+    outline: {
+      color: 'primary',
+      bg: 'transparent',
+      boxShadow: 'inset 0 0 0 2px'
+    },
+  },
 }
 
-export default App;
+function test(props) {
+  function handleClick(e){
+    e.preventDefault()
+    console.log("TEST")
+  }
+  return(
+    <Button variant='primary' onClick={handleClick}>Beep</Button>
+  )
+}
+
+export default props =>
+  <ThemeProvider theme={theme}>
+    {props.children}
+    {test()}
+  </ThemeProvider>
